5ad4117653e9cde9c1ceb35ecd040fdcb2c7a42f
Amit Kucheria <amit.kucheria@linaro.org>
2019-01-30 02:45:43 +0530

    build-buildroot.sh: improve shell environment
    - Add some aliases
    - Setup PATH
    - Add a script to setup the board (S99qclt)
    
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>

--- build-buildroot.sh
+++ build-buildroot.sh
@@ -15,8 +15,45 @@ printf "%s" \
 CONFIG_FEATURE_TASKSET_FANCY=y
 ' > busybox.fragment
 
+# Set up shell environment
+printf "%s" \
+       '# Prompt
+PS1="--o \u@\h o--(\w) \$ "
+
+# Aliases
+alias la="ls -a"
+alias ll="ls -lh"
+alias cls="clear"
+alias ..="cd .."
+alias ...="..;.."
+alias ....="...;.."
+alias gr="grep --color=auto"
+alias grw="grep -w --color=auto"
+alias goog="ping www.google.com"
+' > system/skeleton/etc/profile.d/shell.sh
+
+mkdir -p system/skeleton/etc/init.d
+printf "%s" \
+'#!/bin/sh
+
+# Foo
+touch /tmp/foo
+
+# Load wifi module
+modprobe ath10k-snoc
+
+# Configure Cherokee
+/usr/local/bin/qrtr-cfg 1 && /usr/local/bin/qrtr-ns
+' > system/skeleton/etc/init.d/S99qclt
+
+printf "%s" \
+'/etc/init.d/S99qclt          f       755    0       0       -       -       -       -       -
+' > system/qclt_device_table.txt
+
 printf "%s" \
 'BR2_aarch64=y
+BR2_SYSTEM_DEFAULT_PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/root/bin"
+BR2_ROOTFS_DEVICE_TABLE="system/device_table.txt system/qclt_device_table.txt"
 BR2_TOOLCHAIN_EXTERNAL=y
 BR2_PACKAGE_BUSYBOX_CONFIG_FRAGMENT_FILES="busybox.fragment"
 BR2_ROOTFS_DEVICE_CREATION_DYNAMIC_EUDEV=y
@@ -51,8 +88,8 @@ rm -rf tmp-scripts
 
 mkdir -p tmp-scripts/etc
 mkdir -p tmp-scripts/etc/udev/rules.d
-mkdir -p tmp-scripts/etc/profile.d
-mkdir -p tmp-scripts/root
+mkdir -p tmp-scripts/etc/init.d
+mkdir -p tmp-scripts/root/bin
 
 sed 's,console::respawn:/sbin/getty -L  console 0 vt100 # GENERIC_SERIAL,console::respawn:-/bin/sh,g' $BUILDROOT_TREE/output/target/etc/inittab > tmp-scripts/etc/inittab
 
@@ -62,13 +99,6 @@ sed -i '\,::sysinit:/bin/mount -a,a ::sysinit:/bin/ln -s /sys/kernel/tracing /tr
 sed -i '\,::sysinit:/bin/mount -a,a ::sysinit:/bin/mount -t tracefs nodev /sys/kernel/tracing' tmp-scripts/etc/inittab
 
 touch tmp-scripts/etc/udev/rules.d/80-net-name-slot.rules
-
-# Set up shell environment
-printf "%s" \
-'
-PS1="--o \u@\h o--(\w) \$ "
-
-' > tmp-scripts/etc/profile.d/shell.sh
 
 cd tmp-scripts
 find . | cpio -o -H newc | gzip -9 > $ROOTFSTWEAKS_CPIO
